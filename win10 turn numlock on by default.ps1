7aae9cb2cb220e343e74f510221ac763ea306f27
necessaryMountain <83309232+necessaryMountain@users.noreply.github.com>
2021-10-27 15:25:46 -0400

    Add files via upload
    Changes a value in the registry to have win10 turn on numlock by default.  Disable fast startup as well for the best results.

---
+++ win10 turn numlock on by default.ps1
@@ -0,0 +1,4 @@
+﻿$regName = 'InitialKeyboardIndicators'
+$value = Get-ItemProperty -Path 'Registry::HKEY_USERS\.DEFAULT\Control Panel\Keyboard' -Name $regName
+$newValue = '2'
+Set-ItemProperty -Path 'Registry::HKEY_USERS\.DEFAULT\Control Panel\Keyboard' -Name $regName -Value $newValue
