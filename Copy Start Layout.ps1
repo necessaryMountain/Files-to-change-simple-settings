45699cf1e7b8918ec11fc04654d77c1ebdd28b96
necessaryMountain <83309232+necessaryMountain@users.noreply.github.com>
2021-10-28 08:21:24 -0400

    Add files via upload
    This script is designed to change the start menu layout on a windows 10 machine from a file you've created.  The script is run with our rmm agent which is where it gets those arguments.  It creates a local directory to put the file in locally and runs that local file to change the start menu.  Change things around to suit your needs if you want to use this script.

---
+++ Copy Start Layout.ps1
@@ -0,0 +1,6 @@
+﻿$localDir = ConvertFrom-StringData -StringData $args[0]
+$networkDir = ConvertFrom-StringData -StringData $args[1]
+$layoutFileName = ConvertFrom-StringData -StringData $args[2]
+New-Item -ItemType directory -Path $localDir -Force
+Copy-Item -Path $networkDir -Destination $localDir -Force
+Import-StartLayout -LayoutPath $localDir + $layoutFileName -MountPath $env:SystemDrive\
