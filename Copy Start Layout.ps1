bb2f9a4fd2f98faf714c3fb2a1bbc9ac915d44a9
necessaryMountain <83309232+necessaryMountain@users.noreply.github.com>
2021-10-28 08:32:17 -0400

    Delete Copy Start Layout.ps1

--- Copy Start Layout.ps1
+++
@@ -1,6 +0,0 @@
-﻿$localDir = ConvertFrom-StringData -StringData $args[0]
-$networkDir = ConvertFrom-StringData -StringData $args[1]
-$layoutFileName = ConvertFrom-StringData -StringData $args[2]
-New-Item -ItemType directory -Path $localDir -Force
-Copy-Item -Path $networkDir -Destination $localDir -Force
-Import-StartLayout -LayoutPath $localDir + $layoutFileName -MountPath $env:SystemDrive\
