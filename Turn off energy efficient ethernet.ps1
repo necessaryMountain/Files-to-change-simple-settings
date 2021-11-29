0ad48841456570f045b8b834cf25381240b593e7
necessaryMountain <83309232+necessaryMountain@users.noreply.github.com>
2021-11-29 13:21:28 -0500

    Add files via upload

---
+++ Turn off energy efficient ethernet.ps1
@@ -0,0 +1,5 @@
+﻿#Sets the energy efficient ethernet adapter property to "off" or "disabled"
+
+Set-NetAdapterAdvancedProperty -Name "Ethernet" -DisplayName "Energy Efficient Ethernet" -DisplayValue "OFF"
+
+Set-NetAdapterAdvancedProperty -Name "Ethernet" -DisplayName "Energy-Efficient Ethernet" -DisplayValue "Disabled" 
