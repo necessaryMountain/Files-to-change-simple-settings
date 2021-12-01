68a0f224b230e751b35bbc8b1400a1ce56f13d6d
necessaryMountain <83309232+necessaryMountain@users.noreply.github.com>
2021-12-01 15:32:48 -0500

    Add files via upload
    Removes all Microsoft Store apps from Windows 10 and makes it so new users won't install new apps either.

---
+++ Remove MSstore Apps.ps1
@@ -0,0 +1,9 @@
+﻿Get-AppxPackage -AllUsers | where-object {$_.name –notlike “*store*”} | Remove-AppxPackage
+
+#Credit to thelazyadmin's Rudy Mens.  I modified their code to look for all apps that aren't the Store instead of a single app.
+Get-AppXProvisionedPackage -Online | where-object {$_.name -notlike "*store*"} | Remove-AppxProvisionedPackage -Online
+            
+$appPath="$Env:LOCALAPPDATA\Packages\$app*"
+Remove-Item $appPath -Recurse -Force -ErrorAction 0
+
+Get-appxprovisionedpackage –online | where-object {$_.packagename –notlike “*store*”} | Remove-AppxProvisionedPackage -online
